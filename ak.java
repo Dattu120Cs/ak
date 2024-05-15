62dc441611e782511c763bfebb30a2188ab31bdb
Dattu120Cs <165632352+Dattu120Cs@users.noreply.github.com>
2024-05-15 20:58:16 +0530

    Update ak.java

--- ak.java
+++ ak.java
@@ -2,5 +2,6 @@ public class ak {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to gpt college");
-        System.out.println("welcome gpt college");
+                System.out.println("welcome gpt college");
 	}
+}
